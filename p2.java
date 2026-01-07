babf5414dbd010c03bc72b72d2f9262180bdc708
preranaambilwade034 <preranaambilwade034@gmail.com>
2026-01-07 16:39:48 +0530

    My codebase version1

---
+++ p2.java
@@ -0,0 +1,9 @@
+public class SmallAddition {
+    public static void main(String[] args) {
+        int num1 = 5;   // first number
+        int num2 = 7;   // second number
+        int sum = num1 + num2;  // addition
+
+        System.out.println("Sum: " + sum);
+    }
+}
